cf124982bb9db817c597fa62c00454b88a116af8
lipokli <li6683222@qq.com>
2019-07-22 11:07:08 +0800

    helloworld

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+		System.out.println("Hello lipok");
+		System.out.println("Hello 001");
+		System.out.println("Hello 0022");
+    }
+}
